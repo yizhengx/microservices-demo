d941809292c2361765927644cf0217484c59f9e7
yizhengx <yizhengx@bu.edu>
2024-11-03 09:40:31 -0500

    Nit build script

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 for folder in $(ls -d */ | sed 's:/$::'); do
     cd $folder
     # echo "$folder"
-    sudo docker build -t yizhengx/online-boutique-v0.3.9:$folder . -- push
+    sudo docker build -t yizhengx/online-boutique-v0.3.9:$folder . --push
     # echo "sudo docker build -t yizhengx/online-boutique-v0.3.9:$folder . -- push"
     cd ..
 done
